937c21168bb4288e9bc7f802c51892803703cd0b
maarikam <maarika.markus@nortal.com>
2021-12-01 17:49:23 +0200

    Refactor into part I and part II for clarity.

--- 2021/1.py
+++ 2021/1.py
@@ -1,44 +1,49 @@
 
 # PART I
-'''previous = None
-counter = 0
+def part1():
+  previous = None
+  counter = 0
 
-with open("1_input.txt") as f:
-  data = [int(line.strip()) for line in f.readlines()]
+  with open("1_input.txt") as f:
+    data = [int(line.strip()) for line in f.readlines()]
 
-  for el in data:
-    if previous == None:
+    for el in data:
+      if previous == None:
+        previous = el
+        continue
+
+      if el > previous:
+        counter += 1
+      
       previous = el
-      continue
 
-    if el > previous:
-      counter += 1
-    
-    previous = el
-
-print(counter)'''
+  return counter
 
 
 # PART II
-previous = None
-counter = 0
+def part2():
+  previous = None
+  counter = 0
 
-with open("1_input.txt") as f:
-  data = [int(line.strip()) for line in f.readlines()]
+  with open("1_input.txt") as f:
+    data = [int(line.strip()) for line in f.readlines()]
 
-  for i in range(len(data)):
-    if i >= len(data) - 2:
-      break
-    
-    window_sum = data[i] + data[i+1] + data[i+2]
+    for i in range(len(data)):
+      if i >= len(data) - 2:
+        break
+      
+      window_sum = data[i] + data[i+1] + data[i+2]
 
-    if previous == None:
+      if previous == None:
+        previous = window_sum
+        continue
+
+      if window_sum > previous:
+        counter += 1
+
       previous = window_sum
-      continue
 
-    if window_sum > previous:
-      counter += 1
+  return counter
 
-    previous = window_sum
-    
-print(counter)
+print("Part I", part1())      
+print("Part II", part2())      
